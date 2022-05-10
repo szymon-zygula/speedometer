58d3554de7c30799e8f3b58e195e926e5324fdea
Szymon Zyguła <szymon@zygula.eu>
2022-05-10 08:21:24 +0200

    Add some empty functions

--- web/script.js
+++ web/script.js
@@ -1,3 +1,31 @@
+const CANVAS_SIZE = 300;
+const SERVER_ADDRESS = "localhost:1234";
+
+const initCanvas = () => {
+    const canvas = document.getElementById("graph-canvas");
+    canvas.width = CANVAS_SIZE;
+    canvas.height = CANVAS_SIZE;
+
+    if (!canvas.getContext) {
+        alert("Canvas nie jest wspierany");
+        return;
+    }
+
+    return canvas.getContext("2d");
+}
+
+const getNewData = () => {
+    // Get data from server
+}
+
+const drawPlot = (ctx, xs, ys) => {
+
+};
+
+const drawAxes = (ctx) => {
+
+};
+
 const drawLine = (ctx, x1, y1, x2, y2) => {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -5,13 +33,8 @@ const drawLine = (ctx, x1, y1, x2, y2) => {
 };
 
 const main = () => {
-    const canvas = document.getElementById("graph-canvas");
-
-    if (!canvas.getContext) {
-        alert("Canvas nie jest wspierany");
-        return;
-    }
-
-    const ctx = canvas.getContext("2d");
+    const ctx = initCanvas();
     drawLine(ctx, 0, 0, 20, 100);
 };
+
+main();
