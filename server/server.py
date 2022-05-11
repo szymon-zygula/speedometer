c303365656053dabb566f47769f9565148ff216a
Szymon Zyguła <szymon@zygula.eu>
2022-05-11 19:29:05 +0200

    Add some things

--- server/server.py
+++ server/server.py
@@ -4,14 +4,25 @@ import tornado.web
 PORT = 8888
 
 
-class MainHandler(tornado.web.RequestHandler):
+class DataWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
+class StaticHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('Hello')
+        self.write('index.html !!!')
 
 
 def make_app():
     return tornado.web.Application([
-        (r'/', MainHandler)
+        (r'/', StaticHandler)
     ])
 
 
